b86b177f34f62c5a8ba8f6130406a32714527e04
Andre Ryser <andre.ryser@gmail.com>
2016-12-15 23:45:32 +0100

    Only some tman stuff to do

--- qc.erl
+++ qc.erl
@@ -20,7 +20,7 @@
 -compile(export_all).
 
 
--define(T, 8). % Number of bites of the identifier
+-define(T, 3). % Default: 8, Number of bites of the identifier
 
 hash(Identifier) ->
     <<X:256/big-unsigned-integer>> = crypto:hash(sha256, Identifier),
@@ -43,7 +43,35 @@ predecessor(Self) -> Self#node.predecessor.
 
 new_node() ->
     Id = gen_node_id(),
+    io:format("Created new Node with id: ~p~n", [Id]),
     #node{nodeid=Id, data=dict:new()}.
+new_node(Id) ->
+    io:format("Created new Node with id: ~p~n", [Id]),
+    #node{nodeid=Id, data=dict:new()}.
+
+id({Id, _}) -> Id.
+proc({_, Proc}) -> Proc.
+
+printNode(Node) ->
+    try
+        io:format("~nNode ~p: ~n", [Node#node.nodeid]),
+        io:format("~cSucessor: ~p~n", [9, id(hd(Node#node.fingers))]),
+        io:format("~cPredecessor: ~p~n", [9, id(Node#node.predecessor)]),
+        PrintFinger = fun({Idx, Finger}) -> printFinger(Node, Idx, Finger) end,
+        Fingers = Node#node.fingers,
+        IndexedFingers = lists:zip(lists:seq(1, length(Fingers)), Fingers),
+        lists:map(PrintFinger, IndexedFingers),
+        io:format("~n~n")
+    catch
+        exit:function_clause -> io:format("Error printing node~n");
+        exit:badarg -> io:format("Error printing node~n");
+        _:_ -> io:format("Error printing node: unhandled exception~n")
+    end.
+
+printFinger(Node, Idx, {FingerId, _}) ->
+    Start = finger_start(Node, Idx),
+    End = finger_start(Node, Idx+1),
+    io:format("~cstart: ~p, range: [~p, ~p), succ:~p~n", [9, Start, Start, End, FingerId]).
 
 init_initial_node(Self) ->
     MyId = Self#node.nodeid,
@@ -57,22 +85,38 @@ node_await(Self) ->
     MyId = Self#node.nodeid,
     receive
         {getid, FromProc} ->
-            FromProc ! {gotId, MyId};
+            io:format("Node ~p: Got GetId Query~n", [MyId]),
+            FromProc ! {gotid, MyId};
         {getsucc, FromProc} ->
+            io:format("Node ~p: Got GetSucc Query~n", [MyId]),
             FromProc ! {gotsucc, successor(Self)};
         {getpred, FromProc} ->
+            io:format("Node ~p: Got GetPred Query~n", [MyId]),
             FromProc ! {gotpred, predecessor(Self)};
         {closestprecedingfinger, FromProc, Id} ->
+            io:format("Node ~p: Got ClosestPrecedingFinger Query~n", [MyId]),
             FromProc ! {gotclosestprecedingfinger, closest_preceding_finger(Self, Id)};
         {findpred, From, Id} ->
-            Pred = find_predecessor(Self, {MyId, self()}, Id),
+            io:format("Node ~p: Got FindPred Query~n", [MyId]),
+            Pred = find_predecessor(Self, Id),
             From ! {foundpred, Pred};
         {findsucc, From, Id} ->
+            io:format("Node ~p: Got FindSuccessor Query~n", [MyId]),
             Succ = find_successor(Self, Id),
             From ! {foundsucc, Succ};
         {newpred, Pred} ->
-            node_await(Self#node{predecessor=Pred})
-    end.
+            io:format("Node ~p: Got New Predecessor Update Query~n", [MyId]),
+            node_await(Self#node{predecessor=Pred});
+        {updatefinger, NewNode, Idx} ->
+            io:format("Node ~p: Got UpdateFinger Query with Index ~p~n", [MyId, Idx]),
+            NewSelf = update_finger(Self, self(), NewNode, Idx),
+            node_await(NewSelf);
+        {printstate} ->
+            io:format("Node ~p: Got PrintState Query~n", [MyId]),
+            %io:format("~p~n~n", [Self])
+            printNode(Self)
+    end,
+    node_await(Self).
 
 get_pred(Self, {_, NodeProc}) when NodeProc =:= self() ->
     predecessor(Self);
@@ -100,8 +144,10 @@ get_id(NodeProc) ->
     end.
 
 get_closest_preceding_finger(Self, {_, NodeProc}, Id) when NodeProc =:= self() ->
+    io:format("get_closest_preceding_finger on self~n"),
     closest_preceding_finger(Self, Id);
 get_closest_preceding_finger(_, {_, NodeProc}, Id) ->
+    io:format("get_closest_preceding_finger rpc~n"),
     NodeProc ! {closestprecedingfinger, self(), Id},
     receive
         {gotclosestprecedingfinger, NewNode} -> NewNode
@@ -109,17 +155,46 @@ get_closest_preceding_finger(_, {_, NodeProc}, Id) ->
 
 % {findsucc, From, Id} Find successor to node with id
 
-find_predecessor(Self,{PredId, PredProc}, Id) ->
+find_predecessor(Self, Id) ->
+    io:format("Find predecessor called on self()~n"),
+    MyId = Self#node.nodeid,
+    findpred(Self, {MyId, self()}, Id).
+
+find_predecessor(_, {_, SomeNodeProc}, Id) ->
+    io:format("Find predecessor called for rpc~n"),
+    SomeNodeProc ! {findpred, self(), Id},
+    receive
+        {foundpred, Pred} ->
+            Pred
+    end.
+
+findpred(Self, {PredId, PredProc}, Id) ->
+    io:format("~nFindPred with n'=~p and id=~p called~n", [PredId, Id]),
+    io:format("With Node: "),
+    printNode(Self),
     {SuccToPredId, _} = get_succ(Self, {PredId, PredProc}),
+    io:format("Successor: ~p~n~n", [SuccToPredId]),
+    IsBetween = is_betweenOC(Id, PredId, SuccToPredId),
+    io:format("Is Id(~p) Between PredId(~p) and his Successor(~p)~n", [Id, PredId, SuccToPredId]),
     if
-        PredId < Id  andalso Id =< SuccToPredId -> {PredId, PredProc};
-        true -> find_predecessor(Self, get_closest_preceding_finger(Self, {PredId, PredProc}, Id), Id)
+        IsBetween -> {PredId, PredProc};
+        true ->
+            ClosestPrecFinger = get_closest_preceding_finger(Self, {PredId, PredProc}, Id),
+            io:format("Closest Preceeding Finger: ~p~n", [ClosestPrecFinger]),
+            findpred(Self, ClosestPrecFinger, Id)
     end.
 
 find_successor(Self, Id) ->
-    MyId = Self#node.nodeid,
-    Succ = find_predecessor(Self, {MyId, self()}, Id),
-    get_succ(Self, Succ).
+    io:format("Find successor called on self()~n"),
+    Pred = find_predecessor(Self, Id),
+    get_succ(Self, Pred).
+find_successor(_, {_, OtherProc}, Id) ->
+    io:format("Find successor sends rpc~n"),
+    OtherProc ! {findsucc, self(), Id},
+    receive
+        {foundsucc, Succ} ->
+            Succ
+    end.
 
 % Node has the new predecessor Pred
 send_new_predecessor(Self, {_, NodeProc}, Pred) when NodeProc =:= self() ->
@@ -128,7 +203,27 @@ send_new_predecessor(Self, {_,NodeProc}, Pred) ->
     NodeProc ! {newpred, Pred},
     Self.
 
-send_new_finger()
+
+update_finger(Self, RecipientProc, NewNode, Index) when RecipientProc =:= self() ->
+    MyId = Self#node.nodeid,
+    {NewNodeId,_} = NewNode,
+    {NthFingerId, _} = lists:nth(Index, Self#node.fingers),
+    IsBetween = is_betweenCC(NewNodeId, MyId, NthFingerId),
+    if
+        IsBetween ->
+            io:format("Updated Finger[~p], New Node(~p) between me(~p) and current finger(~p)~n", [Index, NewNodeId, MyId, NthFingerId]),
+            Fingers = setNthElem(Self#node.fingers, Index, NewNode),
+            {_, PredProc} = Self#node.predecessor,
+            update_finger(Self, PredProc, NewNode, Index),
+            Self#node{fingers=Fingers};
+        true ->
+            io:format("Not Updated Finger[~p], New Node(~p) not between me(~p) and current finger(~p)~n", [Index, NewNodeId, MyId, NthFingerId]),
+            Self
+    end;
+update_finger(Self, RecipientProc, NewNode, Index) ->
+    RecipientProc ! {updatefinger, NewNode, Index},
+    Self.
+
 
 % {foundsucc, Succ} The response to above query
 % {findpred, From, Id} Find predecessor to node with id
@@ -136,42 +231,70 @@ send_new_finger()
 
 get_fingers(Self, {LastFingerId, LastFingerProc}, Idx) ->
     MyId = Self#node.nodeid,
-    Start = finger_start(Self, 1),
+    Start = finger_start(Self, Idx),
     ReuseLastFinger = is_between(Start, MyId, LastFingerId),
     NewFinger = if
         ReuseLastFinger -> {LastFingerId, LastFingerProc};
         true -> find_successor(Self, Start)
     end,
     if
-        Idx == ?T-1 -> [NewFinger];
+        Idx == ?T -> [NewFinger];
         true -> [NewFinger|get_fingers(Self, NewFinger, Idx+1)]
     end.
 
-init_finger_table(Self) ->
-    Successor = find_successor(Self, finger_start(Self, 1)),
+init_finger_table(Self, SomeNode) ->
+    Successor = find_successor(Self, SomeNode, finger_start(Self, 1)),
     Predecessor = get_pred(Self, Successor),
-    send_new_predecessor(Self, Successor, {Self#node.nodeid, self()}),
-    Fingers = [Successor|get_fingers(Self, Successor, 2)],
-    Self#node{predecessor=Predecessor, fingers=Fingers}.
+    Self2 = Self#node{predecessor=Predecessor, fingers=[Successor]},
+    send_new_predecessor(Self2, Successor, {Self2#node.nodeid, self()}),
+    Fingers = [Successor|get_fingers(Self2, Successor, 2)],
+    Self2#node{predecessor=Predecessor, fingers=Fingers}.
 
+update_others(Self, Idx) ->
+    MyId = Self#node.nodeid,
+    MyNode = {MyId, self()},
+    SearchId = mod(1 + MyId - (1 bsl (Idx-1)), (1 bsl ?T)), % TODO 1 +... because otherwise we dont find nodeid=searchid
+    {PredId, PProc} = find_predecessor(Self, SearchId),
+    io:format("Node ~p: Predecessor to SearchId(~p) is ~p~n", [MyId, SearchId, PredId]),
+    update_finger(Self, PProc, MyNode, Idx),
+    if
+        Idx == ?T ->
+            pass;
+        Idx =< ?T -> update_others(Self, Idx+1)
+    end.
 
 
 join(Self, SomeNode) ->
-    Self = init_finger_table(Self),
+    Self2 = init_finger_table(Self, SomeNode),
+    update_others(Self2, 1),
+    node_await(Self2).
+
+%% ===== FOR TMAN ===== %%
+buildFullNode(NodeId, Predecessor, SuccNeighbors) ->
+
+    pass.
+
+buildFinger(Idx, SuccNeighbors, LastFinger) ->
     pass.
 
 %% ===== FUNCTIONS ===== %%
 
 finger_start(Self, Idx) ->
     MyId = Self#node.nodeid,
-    MyId + (1 bsl (Idx-1)).
+    Start = MyId + (1 bsl (Idx-1)),
+    modRing(Start).
 
 closest_preceding_finger(Self, Id) ->
     get_closest_finger(lists:reverse(Self#node.fingers), Self#node.nodeid, Id).
 
 
+get_closest_finger([], _, _) ->
+    io:format("Error: get_closest_finger got empty list~n"),
+    erlang:error(fingers_empty);
+get_closest_finger([Finger], _, _) -> Finger;
 get_closest_finger([{FingerId,FingerPID}|Fingers], MyId, Id) ->
-    IsBetween = is_between(FingerId, MyId, Id),
+    io:format("closest_finger: FingerId(~p) between me(~p) and id(~p)~n", [FingerId, MyId, Id]),
+    IsBetween = is_betweenCC(FingerId, MyId, Id) andalso FingerId /= MyId,
     if
         IsBetween ->
             {FingerId,FingerPID};
@@ -180,15 +303,52 @@ get_closest_finger([{FingerId,FingerPID}|Fingers], MyId, Id) ->
     end.
 
 
-is_between(Id, From, To) when From =< To ->
+is_between(_, From, To) when From == To -> true;
+is_between(Id, From, To) when From < To ->
     Id >= From andalso Id =< To;
 is_between(Id, From, To) when From > To ->
     Id >= From orelse Id =< To.
 
+% Open Closed: Id != From, but Id==To
+% If From = To we assume the whole circle is meant
+is_betweenOC(_, From, To) when From == To -> true;
+is_betweenOC(Id, From, To) when From < To ->
+    Id > From andalso Id =< To;
+is_betweenOC(Id, From, To) when From > To ->
+    Id > From orelse Id =< To.
+
+
+is_betweenCC(_, From, To) when From == To -> true;
+is_betweenCC(Id, From, To) when From < To ->
+    Id > From andalso Id < To;
+is_betweenCC(Id, From, To) when From > To ->
+    Id > From orelse Id < To.
+
+mod(X,Y) -> (X rem Y + Y) rem Y.
+modRing(X) -> mod(X, (1 bsl ?T)).
+
+setNthElem(L, Idx, Elem) ->
+    lists:sublist(L, Idx-1) ++ [Elem] ++ lists:nthtail(Idx, L).
+
 master() ->
-    InitialProc = spawn(?MODULE, init_initial_node, [new_node()]),
+    random:seed(now()),
+    InitialProc = spawn(?MODULE, init_initial_node, [new_node(0)]),
     InitialId = get_id(InitialProc),
-    SecondProc = spawn(?MODULE, join, [new_node(), {InitialId, InitialProc}]),
+    SecondProc = spawn(?MODULE, join, [new_node(1), {InitialId, InitialProc}]),
     SecondId = get_id(SecondProc),
-    io:format("Chord with 2 nodes: ~p(~p), ~p(~p)", [InitialId, InitialProc, SecondId, SecondProc]),
+    ThirdProc = spawn(?MODULE, join, [new_node(3), {InitialId, InitialProc}]),
+    ThirdId = get_id(ThirdProc),
+    ForthProc = spawn(?MODULE, join, [new_node(6), {InitialId, InitialProc}]),
+    ForthId = get_id(ThirdProc),
+    %io:format("Chord with 2 nodes: ~p(~p), ~p(~p)", [InitialId, InitialProc, SecondId, SecondProc]),
+
+    timer:sleep(200),
+    InitialProc ! {printstate},
+    timer:sleep(200),
+    SecondProc ! {printstate},
+    timer:sleep(200),
+    ThirdProc ! {printstate},
+    timer:sleep(200),
+    ForthProc ! {printstate},
+    timer:sleep(200),
     pass.
--- tman.erl
+++ tman.erl
@@ -3,15 +3,18 @@
 
 -compile(export_all).
 
--define(N, 10). % Number of nodes
+-define(N, 4). % Number of nodes
 -define(T, 8). % Number of bites of the identifier
--define(M, 2). % Message size for T-Man
--define(NBINIT, 5). % Number of initial neighbors
+-define(M, 2). % Message size for T-Man(even number)
+-define(NBINIT, 2). % Number of initial neighbors
 -define(CycleTimeMs, 200). % The cycle interval for the T-Man to ask neighbors in Miliseconds
+-define(NBCycles, 15).
+
+-import(qc, [buildFullNode/3]).
 
 hash(Identifier) ->
     <<X:256/big-unsigned-integer>> = crypto:hash(sha256, Identifier),
-    X rem (1 bsl ?T). % calculates 2^m
+    X rem (1 bsl ?T). % calculates 2^T
 
 gen_node_id() ->
     Salt = atom_to_list(node()),
@@ -23,20 +26,35 @@ gen_node_id() ->
 % Gossip Response: {gossip2, {SuccesorsNeirestToRecipient, PredecessorsNeirestToRecipient}
 % Next Cycle Message: {nextcycle}
 
+distributeSubsets([], _) -> done;
+distributeSubsets([Node|Nodes], AllNodes) ->
+    {_, Pid} = Node,
+    Pid ! {init, randomSubset(lists:delete(Node, AllNodes), ?NBINIT)},
+    distributeSubsets(Nodes, AllNodes).
+
 master() ->
     random:seed(now()),
-    NodeIds = [gen_node_id() || list:seq(1,?N)],
+    NodeIds = [gen_node_id() || _ <- lists:seq(1,?N)],
     Nodes = [ {NodeId, spawn(?MODULE, node_init, [NodeId])} || NodeId <- NodeIds],
-
     % send to each node a random subset of nodes (initial neighbors)
-    [Pid ! {init, randomSubset(Nodes, ?NBINIT)} || Pid <- Nodes].
+    distributeSubsets(Nodes, Nodes),
+    %[Pid ! {init, randomSubset(Nodes, ?NBINIT)} || {_,Pid} <- Nodes],
+
+    timer:sleep(200),
+    {_,FirstProc} = hd(Nodes),
+    FirstProc ! {printstate},
+    timer:sleep(200),
+    pass.
 
 node_init(NodeId) ->
     receive
         {init, NodeSubset} ->
-            cycle(NodeId, sortByDist(NodeSubset, NodeId), 1)
+            cycle(NodeId, sortByDist(NodeSubset, NodeId), ?NBCycles)
     end.
 
+cycle(NodeId, Neighbors, 1) ->
+    Pred = hd(sortByPredDist(Neighbors, NodeId)),
+    buildFullNode(NodeId, Pred, sortBySuccDist(Neighbors, NodeId));
 cycle(NodeId, Neighbors, CycleNr) ->
     {RandomNeighborId, RandomNeighborProc} = randomElem(Neighbors),
     WaitMs = random:uniform(?CycleTimeMs),
@@ -49,32 +67,49 @@ cycle_waiting(NodeId, Neighbors, CycleNr) ->
     receive
         {gossip, FromId, FromProc, {NewSuccs, NewPreds}} ->
             FromProc ! {gossip2, nearestNeighbors(Neighbors, FromId)},
-            cycle_waiting(NodeId, sortByDist(lists:concat([Neighbors, NewSuccs, NewPreds]), NodeId), CycleNr);
+            %cycle_waiting(NodeId, sortByDist(lists:concat([Neighbors, NewSuccs, NewPreds]), NodeId), CycleNr);
+            cycle_waiting(NodeId, mergeNeighbors([Neighbors, NewSuccs, NewPreds], NodeId), CycleNr);
         {gossip2, {NewSuccs, NewPreds}} ->
-            cycle_waiting(NodeId, sortByDist(lists:concat([Neighbors, NewSuccs, NewPreds]), NodeId), CycleNr);
+            %cycle_waiting(NodeId, sortByDist(lists:concat([Neighbors, NewSuccs, NewPreds]), NodeId), CycleNr);
+            cycle_waiting(NodeId, mergeNeighbors([Neighbors, NewSuccs, NewPreds], NodeId), CycleNr);
         {nextcycle} ->
-            cycle(NodeId, Neighbors, CycleNr+1)
+            cycle(NodeId, Neighbors, CycleNr-1);
+        {printstate} ->
+            printTmanNode(NodeId, Neighbors),
+            cycle_waiting(NodeId, Neighbors, CycleNr)
     end.
+
 
 %% ===== FUNCTIONS ===== %%
 
+printTmanNode(NodeId, Neighbors) ->
+    io:format("~nNode ~p has Neighbors:~n", [NodeId]),
+    lists:map(fun({Id,_}) -> io:format("~c~p~n", [9, Id]) end, Neighbors),
+    io:format("~n~n").
+
+mergeNeighbors(Lists, NodeId) ->
+    lists:delete({NodeId, self()}, sortByDist(lists:concat(Lists), NodeId)).
+
+
 nearestNeighbors(Neighbors, OtherId) ->
-    FromSuccs = lists:sublist(sortBySuccDist(Neighbors, OtherId), ?M / 2),
-    FromPreds = lists:sublist(sortByPredDist(Neighbors, OtherId), ?M / 2),
+    SortedBySucc = sortBySuccDist(Neighbors, OtherId),
+    MsgSize = ?M div 2,
+    FromSuccs = lists:sublist(SortedBySucc, MsgSize),
+    FromPreds = lists:sublist(sortByPredDist(Neighbors, OtherId), ?M div 2),
     {FromSuccs, FromPreds}.
 
 % Sort by distance
 sortByDist(L, Id) ->
-    Compare = fun(A,B) -> dist(A, Id) =< dist(B, Id) end,
-    lists:sort(Compare, L).
+    Compare = fun({A,_},{B,_}) -> dist(A, Id) =< dist(B, Id) end,
+    lists:usort(Compare, L).
 
 sortBySuccDist(L, Id) ->
-    Compare = fun(A, B) -> distSucc(Id, A) =< distSucc(Id, B) end,
-    lists:sort(Compare, L).
+    Compare = fun({A,_}, {B,_}) -> distSucc(Id, A) =< distSucc(Id, B) end,
+    lists:usort(Compare, L).
 
 sortByPredDist(L, Id) ->
-    Compare = fun(A, B) -> distPred(Id, A) =< distPred(Id, B) end,
-    lists:sort(Compare, L).
+    Compare = fun({A,_}, {B,_}) -> distPred(Id, A) =< distPred(Id, B) end,
+    lists:usort(Compare, L).
 
 distSucc(A, B) when B-A >= 0 -> B-A; % (MyId, Successor)
 distSucc(A, B) -> B - A + (1 bsl ?T).
@@ -86,16 +121,15 @@ distPred(A, B) -> A - B + (1 bsl ?T).
 dist(A, B) -> min(abs(A-B), (1 bsl ?T)-abs(A-B)).
 
 randomElem(L) ->
-    Idx = random:uniform(list:length(L)),
+    Idx = random:uniform(length(L)),
     lists:nth(Idx, L).
-
 
 % Use the reservoir algorithm to choose a random subset of K nodes
 randomSubset(NodesList, K) when length(NodesList) < K->
     erlang:error("Sample Size bigger than Population");
 randomSubset(NodesList, K) ->
     {Sample, Rest} = lists:split(K, NodesList),
-    randomSubset(Sample, Rest, K, K).
+    randomSubset(Sample, Rest, K, K+1).
 
 randomSubset(Sample, [], _, _) ->
     Sample;
@@ -111,3 +145,20 @@ randomSubset(Sample, [X|Remaining], K, I) ->
 
 setNthElem(L, Idx, Elem) ->
     lists:sublist(L, Idx-1) ++ [Elem] ++ lists:nthtail(Idx, L).
+
+count(Counter, []) -> Counter;
+count(Counter, [Element|Tail]) ->
+    C = lists:nth(Element, Counter),
+    Counter2 = setNthElem(Counter, Element, C+1),
+    count(Counter2, Tail).
+
+
+% Just to make sure that randomSubset works as expected
+testRandomSubset(Rounds, SampleSize) ->
+    Counters = [0 || _ <- lists:seq(1,SampleSize)],
+    testRandomSubset(Counters, Rounds, SampleSize).
+
+testRandomSubset(Counters, 0, _) -> Counters;
+testRandomSubset(Counters, Rounds, SampleSize) ->
+    Counters2 = count(Counters, randomSubset(lists:seq(1, SampleSize), 3)),
+    testRandomSubset(Counters2, Rounds-1, SampleSize).
